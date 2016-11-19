41556829f0a8d256758949c5fc4a84a6bf7146d4
TianYJ1 <yjcbtp@163.com>
2016-11-20 04:49:50 +0800

    Add files via upload

--- m3_linux_task.sh
+++ m3_linux_task.sh
@@ -1,35 +1,5 @@
-#!/bin/bash
-# 
-# Decription: 
-#    A Shell script used to download the imges from Internet.
-# Author:
-#    Long Luo
-# Date:
-#    2014-09-11 00:16:59
-#
- 
-BASE_URL="https://s.yimg.com/zz/combo?a/i/us/nws/weather/gr/"
- 
-# declare STIRNG variable
-STRING="Beginning Image download..."
- 
-#print var on screen 
-echo $STRING
- 
-sleep 1
-echo "...."
- 
-BIG_PNG="ds.png"
-PNG=".png"
- 
-echo "url="${BASE_URL}
-echo "big png="${BIG_PNG}
- 
-for ((i=0; i<49;i++)); do
-  echo IMG_URL=${BASE_URL}${i}${BIG_PNG}
-  echo "final url="${IMG_URL}
- 
-  curl ${BASE_URL}${i}${BIG_PNG} -o small/${i}${PNG}
-  sleep 1
- 
-done
+
+#! /bin/bash
+
+http_proxy=218.107.21.252:8080
+wget http://www.baidu.com -O baidu_html2
