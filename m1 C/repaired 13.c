4506700ea1e7a06f2dd5cd2258ba0b06d8037bf5
TianYJ1 <yjcbtp@163.com>
2017-06-08 21:18:51 +0300

    Create repaired 13.c

--- m1 C/repaired 13.c
+++ m1 C/repaired 13.c
@@ -1,27 +1,33 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <locale.h>
-void copy(char *c,char *x)
-{ char str[1000];
-FILE *C,*X;
 
-C=fopen(c,"r");
-X=fopen(x,"w");
-char i;
-while((i=fgetc(C))!=EOF)
-{
-fputc(i,X);
-}
-}
+#define IN 1
+#define OUT 0
 
-int main()
-{setlocale(LC_CTYPE, "Russian");
-char a[1000],b[1000];
-printf("§£§Ó§Ö§Õ§Ú§ä§Ö §á§å§ä§î §Ü §â§Ñ§ã§á§à§Ý§à§Ø§Ö§ß§Ú§ð §æ§Ñ§Û§Ý§Ñ §Ü§à§ä§à§â§í§Û §Ü§à§á§Ú§â§å§ð§ä(C:\/Games\/filename1.txt)");
-gets(a);
-printf("§£§Ó§Ö§Õ§Ú§ä§Ö §â§Ñ§ã§á§à§Ý§à§Ø§Ö§ß§Ú§Ö §æ§Ñ§Û§Ý§Ñ §Ü§å§Õ§Ñ §Ü§à§á§Ú§â§à§Ó§Ñ§ä§î (C:\/Games\/filename2.txt)");
-gets(b);
-copy(a,b);
-printf("§³§Õ§Ö§Ý§Ñ§ß§à");
-return 0;
+int main(){
+
+	int c,state,length;
+
+	c = state = length = 0;
+	
+	while((c = getchar()) != EOF){
+		
+		if(c == ' '||c == '\n'||c == '\t'){
+			if(state == IN){
+				putchar(':');
+				for(int i = 0; i < length; i++){
+					putchar('*');
+				}
+				putchar('\n');
+			}
+			state = OUT;
+			length = 0;
+		}
+		else {
+			state = IN;
+			putchar(c);
+			length++;
+		}
+		
+	}
+	
 }
