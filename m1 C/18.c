a1c54c727f3ef189f3423211498805200d2cb28f
TianYJ1 <yjcbtp@163.com>
2017-06-18 15:42:33 +0500

    Update 18.c

--- m1 C/18.c
+++ m1 C/18.c
@@ -1,42 +1,27 @@
-#include <stdio.h>
-int Copy_File(char* in_path,char* out_path)
-{
-	FILE* in=NULL;
-	FILE* out=NULL;
-if((in=fopen(in_path,"rb"))==NULL)
-	{
-		printf("Невозможно открыть исходный файл!\n");
-		return(1);
-	}
-	if((out=fopen(out_path,"wb+"))==NULL)
-	{
-		printf("Невозможно создать целевой файл!\n");
-		fclose(in);
-		return(1);
-	}
-	char data;
-	while(!feof(in))
-	{
-		if(1==fread(&data,sizeof(char),1,in))
-			fwrite(&data,sizeof(char),1,out);
-	}
-	fclose(in);
-	fclose(out);
-		
-	return(0);
-}
+#include<stdio.h>
+#define MAX 20
 int main()
 {
-	char in_path[256];
-	char out_path[256];
-printf("Введите путь к исходному файлу: ");
-	scanf("%s",in_path);
-	printf("Введите путь к файлу назначения: ");
-	scanf("%s",out_path);
-	if(Copy_File(in_path,out_path))
-	{
-		printf("Копирование не удалось!");
-	}else
-		printf("успех копирования!");
-	return(0);
+ FILE *fa,*fb;
+ char stra[MAX],strb[MAX],c;
+ printf("Please enter the source file name:");
+ gets(stra);
+ printf("Please enter the target file name:");
+ gets(strb);
+ fa=fopen(stra,"r");
+ fb=fopen(strb,"w");
+ if(fa)
+ {
+  if(fb)
+  {
+   while((c=getc(fa))!=EOF)
+    putc(c,fb);
+   fclose(fa);
+   fclose(fb);
+  }
+  else
+   printf("can't open %s",strb);
+ }
+ else
+  printf("can't open %s",stra);
 }
